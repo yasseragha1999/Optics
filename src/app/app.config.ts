1c2e4a1b922b3bc885dd21404b4f77717dd60ad2
Yasser Agha <yasseragha.dev@gmail.com>
2025-12-10 20:23:00 +0300

    logic navbar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options:{
-          darkModeSelector: '.my-app-dark'
+          // darkModeSelector: '.my-app-dark'
         }
       }
     }),
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
+import {BadgeModule} from 'primeng/badge';
+import {AvatarModule} from 'primeng/avatar';
+import {InputTextModule} from 'primeng/inputtext';
+import {CommonModule} from '@angular/common';
+import {Ripple} from 'primeng/ripple';
+import {Menubar} from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule, Menubar],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Orders',
+        icon: 'pi pi-align-center',
+      },
+      {
+        label: 'Customers',
+        icon: 'pi pi-home',
+      },
+
+    ];
+  }
 }
