34aa3cb11ec5adc7b6b92bf05182729f8ccd68a1
Yasser Agha <yasseragha.dev@gmail.com>
2025-12-10 03:04:49 +0300

    i packges needed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options:{
+          darkModeSelector: '.my-app-dark'
+        }
+      }
+    })
   ]
 };
