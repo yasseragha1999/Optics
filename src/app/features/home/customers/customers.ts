a56c2a667367d91bb92dbe7d9e35a54003fdd7a0
Yasser Agha <yasseragha.dev@gmail.com>
2025-12-15 05:44:39 +0300

    shared data child to parent

--- src/app/features/home/customers/customers.ts
+++ src/app/features/home/customers/customers.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './customers.html',
   styleUrl: './customers.scss',
 })
 export class Customers {
-
 }
